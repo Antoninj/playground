aeeade89f701437f02bd311740cf565e01e3c52f
Neolin <joussonantonin@gmail.com>
2019-11-16 21:44:46 +0100

    add some dependencies

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 from flask import Flask
 from datetime import datetime
+from flask_debugtoolbar import DebugToolbarExtension
+
 app = Flask(__name__)
+
+# the toolbar is only enabled in debug mode:
+app.debug = True
+
+# set a 'SECRET_KEY' to enable the Flask session cookies
+app.config['SECRET_KEY'] = 'mysecret key'
+
+toolbar = DebugToolbarExtension(app)
 
 @app.route('/')
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
     return """
+    <body>
     <h1>Hello heroku</h1>
     <p>This is the latest change</p>
     <p>It is currently {time}.</p>
 
     <img src="http://loremflickr.com/600/400">
+    </body>
     """.format(time=the_time)
-
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
